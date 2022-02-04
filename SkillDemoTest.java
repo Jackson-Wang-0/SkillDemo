dc922882ae5cccce395edcff5fbdeb55e5573ef3
Jackson-Wang-0 <zhw049@ucsd.edu>
2022-02-03 23:05:10 -0800

    fix so the method works now

--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -3,6 +3,6 @@ import org.junit.*;
 public class SkillDemoTest {
     @Test
     public void addition() {
-        assertEquals(2, 1 + 2);//false first
+        assertEquals(2, 1 + 1);//false first
     }
 }
